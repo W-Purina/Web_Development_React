1325fb9c6611aad8e42b7b12bf566fe1b816b846
Purina-p <purina.nz28@gmail.com>
2023-05-08 20:06:15 +0800

    configure change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import UploadPhoto from "./pages/UploadPhoto";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Login />}/>
         <Route path="/register" element={<Register />}/>
         <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+        <Route path="/test" element={<UploadPhoto />}/>
       </Routes>
     </BrowserRouter>
   );
