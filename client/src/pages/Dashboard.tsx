2578d4487f0d8cb7bc163f1d6f195c166f9a6620
Bo Pang <i@ipangbo.cn>
2023-05-12 02:18:59 +1200

    ✨ Add router links in dashboard

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,27 +1,32 @@
-import { FloatingBubble } from "antd-mobile";
+import { FloatingBubble, NavBar } from "antd-mobile";
 import DashboardProfile from "../widgets/DashboardProfile";
 import FamilyCardList from "../widgets/FamilyCardList";
 import { AddOutline } from "antd-mobile-icons";
 import styles from "./Dashboard.module.css";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
   return (
     <>
-      <div className={`${styles.dashboard} relative -z-20 h-full`}>
-        <div className={`${styles.dashboardBG} absolute inset-0 -z-10`}></div>
-        <DashboardProfile></DashboardProfile>
-        {/* Family list */}
-        <FamilyCardList></FamilyCardList>
-        <FloatingBubble
-          style={{
-            "--initial-position-bottom": "24px",
-            "--initial-position-right": "24px",
-            "--edge-distance": "24px",
-            "--background": "orange",
-          }}
-        >
-          <AddOutline fontSize={32}></AddOutline>
-        </FloatingBubble>
+      <NavBar backArrow={false}>Dashboard</NavBar>
+      <div className={`${styles.dashboard} relative h-full`}>
+        <div className={`${styles.dashboardBG} absolute inset-0`}>
+          <DashboardProfile></DashboardProfile>
+          {/* Family list */}
+          <FamilyCardList></FamilyCardList>
+          <FloatingBubble
+            style={{
+              "--initial-position-bottom": "24px",
+              "--initial-position-right": "24px",
+              "--edge-distance": "24px",
+              "--background": "orange",
+            }}
+            onClick={() => navigate("/group/addGroup")}
+          >
+            <AddOutline fontSize={32}></AddOutline>
+          </FloatingBubble>
+        </div>
       </div>
     </>
   );
