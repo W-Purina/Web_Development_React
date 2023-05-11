27c4be0cacfbc52cbf3303b31b43b3f4d9f36914
Bo Pang <i@ipangbo.cn>
2023-05-12 02:17:31 +1200

    ✨ Add Link to Group

--- client/src/components/FamilyCard.tsx
+++ client/src/components/FamilyCard.tsx
@@ -1,5 +1,6 @@
 import { Card } from "antd-mobile";
 import styles from "./FamilyCard.module.css";
+import { Link } from "react-router-dom";
 
 interface FamilyCardProps {
   familyName: string;
@@ -9,18 +10,20 @@ interface FamilyCardProps {
 const FamilyCard = ({ familyName, familyMembers, total }: FamilyCardProps) => {
   return (
     <>
-      <Card title={familyName} className={`${styles.familyCard} mb-8`}>
-        <div className="flex items-center">
-          <div className="flex h-full flex-grow">
-            <span className="pr-3">{familyMembers.join(", ")}</span>
+      <Link to={"/group/1"}>
+        <Card title={familyName} className={`${styles.familyCard} mb-8`}>
+          <div className="flex items-center">
+            <div className="flex h-full flex-grow">
+              <span className="pr-3">{familyMembers.join(", ")}</span>
+            </div>
+            <span
+              className={`${styles.shadowText} text-grey-700 mb-1 h-full text-right text-3xl`}
+            >
+              ${total}
+            </span>
           </div>
-          <span
-            className={`${styles.shadowText} text-grey-700 mb-1 h-full text-right text-3xl`}
-          >
-            ${total}
-          </span>
-        </div>
-      </Card>
+        </Card>
+      </Link>
     </>
   );
 };
